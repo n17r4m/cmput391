c2b425ec1042515cc6be6c190e16137bd9ec96a3
Martin <n17r4m@gmail.com>
2016-03-24 07:34:59 +0000

    Improvements!

--- jaggies.js
+++ jaggies.js
@@ -1,34 +1,54 @@
 
 DB = {
     Users:  Meteor.users,
-    Images: new Meteor.Collection("images")
+    Images: new FS.Collection("images", {
+        stores: [
+            new FS.Store.GridFS("thumbnails", { 
+                beforeWrite: function(fileObj) {
+                    return { extension: 'jpg', type: 'image/jpeg' }
+                },
+                transformWrite: function (fileObj, readStream, writeStream) {
+                    // Transform the image into a 150x150px jpeg thumbnail
+                    gm(readStream, fileObj.name())
+                        .resize(300)
+                        .stream('JPEG')
+                        .pipe(writeStream);
+                }
+            }),
+            new FS.Store.GridFS("images", {
+                beforeWrite: function(fileObj) {
+                    return { extension: 'jpg', type: 'image/jpeg' }
+                },
+                transformWrite: function (fileObj, readStream, writeStream) {
+                    gm(readStream, fileObj.name())
+                        .stream('JPEG')
+                        .pipe(writeStream);
+                }
+            })
+        ],
+        filter: {
+            allow: { contentTypes: ['image/*'] /* allow only images */ }
+        }
+    })
+}
+
+moment.updateLocale('en', {
+    calendar : {
+        sameDay: '[Today]',
+        nextDay: '[Tomorrow]',
+        nextWeek: 'dddd',
+        lastDay: '[Yesterday]',
+        lastWeek: '[Last] dddd',
+        sameElse: 'll'
+    }
+})
+
+if(Meteor.isServer){
+    
+    
 }
 
 if(Meteor.isClient){
     
-    Template.followingMenu.onRendered(function(){
-        
-        $(function() {
-            // fix menu when passed
-            $('.masthead').visibility({
-                once: false,
-                onBottomPassed: function() {
-                    $('.fixed.menu').transition('fade in')
-                },
-                onBottomPassedReverse: function() {
-                    $('.fixed.menu').transition('fade out')
-                }
-            });
-        })
-    })
     
-    Template.sidebarMenu.onRendered(function(){
-
-        $(function() {
-            // create sidebar and attach to menu open
-            $('.ui.sidebar').sidebar('attach events', '.toc.item');
-        })
-    })
-      
-
 }
